c453ff2ade2996005eded643b2addbb3902fe85d
862795799 <862795799@qq.com>
2020-07-14 20:16:34 +0800

    git push

--- test/FizzBuzz.test.js
+++ test/FizzBuzz.test.js
@@ -1,5 +1,26 @@
-import FizzBUzz from '../js/index'
+import FizzBUzz from '../js/index';
 
-test('should 3', () => {
-    expect(FizzBUzz(1,2)).toBe(3)
+test('FizzBuzz', () => {
+  expect(FizzBUzz(3)).toBe('Fizz');
+});
+test('FizzBuzz', () => {
+  expect(FizzBUzz(5)).toBe('Buzz');
+});
+test('FizzBuzz', () => {
+  expect(FizzBUzz(7)).toBe('Whizz');
+});
+test('FizzBuzz', () => {
+  expect(FizzBUzz(13)).toBe('Fizz');
+});
+test('FizzBuzz', () => {
+  expect(FizzBUzz(35)).toBe('BuzzWhizz');
+});
+test('FizzBuzz', () => {
+  expect(FizzBUzz(75)).toBe('Fizz');
+});
+test('FizzBuzz',()=>{
+  expect(FizzBUzz(105)).toBe('BuzzWhizz')
+})
+test('FizzBuzz',()=>{
+  expect(FizzBUzz(30)).toBe('Fizz')
 })
