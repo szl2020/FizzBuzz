1d38323523bda83bbb27a4db0c416fb57ae53d45
862795799 <862795799@qq.com>
2020-07-14 20:18:09 +0800

    project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
-export default function FizzBuzz(a,b){
-    return a+b;
+const reg3 = RegExp(/3/);
+const reg5 = RegExp(/5/);
+const reg7 = RegExp(/7/);
+
+export default function FizzBuzz(num) {
+  if (reg7.test(num)) {
+    if (num % 3 === 0 && num % 7 === 0) {
+      num = 'FizzWhizz';
+    }
+    if (num % 3 === 0) {
+      num = 'Fizz';
+    }
+    if (num % 7 === 0) {
+      num = 'Whizz';
+    }
+    
+  }
+  if (reg5.test(num)) {
+    if (num % 5 === 0 && num % 7 === 0) {
+      num = 'BuzzWhizz';
+    }
+    if (num % 5 === 0) {
+      num = 'Buzz';
+    }
+    if (num % 7 === 0) {
+      num = 'Whizz';
+    }
+   
+  }
+  if (reg3.test(num)) {
+    num = 'Fizz'
+  }
+  if (num % 5 === 0 && num % 7 === 0 && num % 3 == 0) {
+    num = 'FizzBuzzWhizz';
+  }
+  if (num % 3 === 0 && num % 7 === 0) {
+    num = 'FizzWhizz';
+  }
+  if (num % 5 === 0 && num % 7 === 0) {
+    num = 'BuzzWhizz';
+  }
+  if (num % 3 === 0 && num % 7 === 0) {
+    num = 'FizzWhizz';
+  }
+ 
+  if(num % 3 == 0){
+    num = 'Fizz'
+  }
+  if(num % 5 == 0){
+    num = 'Buzz'
+  }
+  if(num % 7 == 0){
+    num = 'Whizz'
+  }
+  return num;
 }
