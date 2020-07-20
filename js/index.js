0bfed33c1f3e1d05e084bbd58b8711106995c92b
862795799 <862795799@qq.com>
2020-07-20 16:20:25 +0800

    v2.0

--- js/index.js
+++ js/index.js
@@ -1,56 +1,87 @@
-const reg3 = RegExp(/3/);
-const reg5 = RegExp(/5/);
-const reg7 = RegExp(/7/);
+const contain3Reg = /3/;
+const contain5Reg =/5/;
+const contain7Reg = /7/;
 
-export default function FizzBuzz(num) {
-  if (reg7.test(num)) {
-    if (num % 3 === 0 && num % 7 === 0) {
-      num = 'FizzWhizz';
+const FIZZ = 'Fizz';
+const BUZZ = 'Buzz';
+const WHIZZ = 'Whizz';
+
+const zhengChu = (num,index) => num % index === 0;
+
+var result = '';
+
+//声明一个对象
+const a7Rule = {
+    isMatch:num=>contain7Reg.test(num),
+    logResult:num=>{
+        result = '';
+        if (zhengChu(num,3)) {
+            result += FIZZ;
+        }
+        if (zhengChu(num,7)) {
+            result += WHIZZ;
+        }
+        return  result;
     }
-    if (num % 3 === 0) {
-      num = 'Fizz';
-    }
-    if (num % 7 === 0) {
-      num = 'Whizz';
-    }
-    
-  }
-  if (reg5.test(num)) {
-    if (num % 5 === 0 && num % 7 === 0) {
-      num = 'BuzzWhizz';
-    }
-    if (num % 5 === 0) {
-      num = 'Buzz';
-    }
-    if (num % 7 === 0) {
-      num = 'Whizz';
-    }
-   
-  }
-  if (reg3.test(num)) {
-    num = 'Fizz'
-  }
-  if (num % 5 === 0 && num % 7 === 0 && num % 3 == 0) {
-    num = 'FizzBuzzWhizz';
-  }
-  if (num % 3 === 0 && num % 7 === 0) {
-    num = 'FizzWhizz';
-  }
-  if (num % 5 === 0 && num % 7 === 0) {
-    num = 'BuzzWhizz';
-  }
-  if (num % 3 === 0 && num % 7 === 0) {
-    num = 'FizzWhizz';
-  }
- 
-  if(num % 3 == 0){
-    num = 'Fizz'
-  }
-  if(num % 5 == 0){
-    num = 'Buzz'
-  }
-  if(num % 7 == 0){
-    num = 'Whizz'
-  }
-  return num;
 }
+const a5Rule = {
+    isMatch:num=>contain5Reg.test(num),
+    logResult:num=>{
+        result = '';
+        if (zhengChu(num,5)) {
+            result += BUZZ;
+        }
+
+        if (zhengChu(num,7)) {
+            result += WHIZZ;
+        }
+        return  result;
+    }
+}
+const a3Rule = {
+    isMatch:num=>contain3Reg.test(num),
+    logResult:num=>{
+        result = '';
+        result = FIZZ;
+        return result;
+    }
+}
+
+const defalutRule = {
+    isMatch:num=>{
+       return true;
+    },//不满足前三种情况
+    logResult:num=>{
+        result = '';
+
+        if ((zhengChu(num,3) && zhengChu(num,5) && zhengChu(num,7) )=== false){
+            result = num;
+
+        }
+        if (zhengChu(num,3)) {
+            result += FIZZ;
+        }
+        if (zhengChu(num,5)) {
+            result += BUZZ;
+        }
+        if (zhengChu(num,7)) {
+            result += WHIZZ;
+        }
+    return result;
+    }
+}
+
+//对于用的比较多的字符串声明常量
+//用的多的地方包装成方法
+//整除3且整除5切整除7可以写成 整除3的话加等于Fizz
+//整合成两种情况 扔到一个对象中 用数组find方法找到第一个满足情况的值，
+
+   export  default function FizzBuzz(num) {
+    return ( [a7Rule,a5Rule,a3Rule,defalutRule].find(ele=>ele.isMatch(num)).logResult(num))
+
+
+
+
+
+}
+FizzBuzz(1);
--- test/FizzBuzz.test.js
+++ test/FizzBuzz.test.js
@@ -1,26 +1,49 @@
 import FizzBUzz from '../js/index';
 
+
+// should throw.../return... when... given...
+
+/*
+ * given 
+ * when
+ * then
+**/
+
+
 test('FizzBuzz', () => {
   expect(FizzBUzz(3)).toBe('Fizz');
 });
+
 test('FizzBuzz', () => {
   expect(FizzBUzz(5)).toBe('Buzz');
 });
+
 test('FizzBuzz', () => {
   expect(FizzBUzz(7)).toBe('Whizz');
 });
+
 test('FizzBuzz', () => {
   expect(FizzBUzz(13)).toBe('Fizz');
 });
+
 test('FizzBuzz', () => {
   expect(FizzBUzz(35)).toBe('BuzzWhizz');
 });
+
 test('FizzBuzz', () => {
   expect(FizzBUzz(75)).toBe('Fizz');
 });
+
 test('FizzBuzz',()=>{
   expect(FizzBUzz(105)).toBe('BuzzWhizz')
-})
+});
+
 test('FizzBuzz',()=>{
   expect(FizzBUzz(30)).toBe('Fizz')
-})
+});
+test('1',()=>{
+    expect(FizzBUzz(1)).toBe(1)
+});
+test('1',()=>{
+    expect(FizzBUzz(2)).toBe(2)
+});
